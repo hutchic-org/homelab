c4ec5ca04ff876343cd5c2e2ba3522d0cb60b814
Colin Hutchinson <chutchic@gmail.com>
2023-03-09 14:58:02 -0500

    feat(mealie): install mealie as a helm chart via an argocd application

--- README.md
+++ README.md
@@ -4,3 +4,18 @@
 cd bootstrap
 cat ~/.kube/config | pulumi config set --secret kubernetes:kubeconfig
 ```
+
+# CRD
+
+```
+kubectl get crd applications.argoproj.io -o yaml > crd/application.yaml
+crd2pulumi --nodejsPath ./crd/application crd/application.yaml
+```
+
+# Applications
+
+## Mealie
+
+```
+https://truecharts.org/charts/common/helm-values/
+```
--- bootstrap/index.ts
+++ bootstrap/index.ts
@@ -1,5 +1,8 @@
 import * as k8s from "@pulumi/kubernetes";
 import * as kx from "@pulumi/kubernetesx";
+import { hostname } from "os";
+import * as application from "./crd/application";
+import { Application } from "./crd/application/argoproj/v1alpha1";
 
 // Create a namespace.
 const ns = new k8s.core.v1.Namespace("argocd", {
@@ -16,3 +19,53 @@ const chart = new k8s.helm.v3.Chart('argocd', {
         repo: 'https://argoproj.github.io/argo-helm',
     },
 })
+
+// Create an ArgoCD Mealie Application.
+
+new k8s.core.v1.Namespace("mealie-ns", {
+    metadata: {
+        name: "mealie",
+    },
+});
+
+new Application("mealie-application", {
+    metadata: {
+        name: "mealie",
+        namespace: "argocd"
+    },
+    spec: {
+        project: "default",
+        source: {
+            chart: "mealie",
+            repoURL: "https://charts.truecharts.org/",
+            targetRevision: "14.0.21",
+            helm: {
+                parameters: [
+                    {
+                        name: "ingress.main.enabled",
+                        value: "true"
+                    },
+                    {
+                        name: "ingress.main.hosts[0].host",
+                        value: "homelab.local"
+                    },
+                    {
+                        name: "ingress.main.hosts[0].paths[0].path",
+                        value: "/"
+                    },
+                    {
+                        name: "ingress.main.enableFixedMiddlewares",
+                        value: "false"
+                    }
+                ]
+            }
+        },
+        destination: {
+            server: "https://kubernetes.default.svc",
+            namespace: "mealie",
+        },
+        syncPolicy: {
+            automated: {}
+        }
+    },
+});
